98a06aca1b78766274eb7e579ad0f843f2eb0180
1020aiKi <133315048+1020aiKi@users.noreply.github.com>
2023-12-14 19:22:42 +0900

    samuraitravel

---
+++ samuraitravel/form/ReputationForm.java
@@ -0,0 +1,15 @@
+package com.example.samuraitravel.form;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+ @Data
+public class ReputationForm { 
+	 
+	private Integer userId;    
+	 
+	private Integer score;
+	
+	@NotBlank(message="文字を入力して下さい")
+	private String description;
+}
---
+++ samuraitravel/repository/ReputationRepository.java
@@ -0,0 +1,11 @@
+package com.example.samuraitravel.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.samuraitravel.entity.Reputation;
+import com.example.samuraitravel.entity.User;
+public interface ReputationRepository extends JpaRepository<Reputation,Integer>{
+	 public Page<Reputation> findByUserOrderByCreatedAtDesc(User user, Pageable pageable);
+}
